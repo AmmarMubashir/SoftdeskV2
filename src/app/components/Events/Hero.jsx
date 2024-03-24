3c4cbc1943dce37dfaf27aa3a161a10ca23932c3
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-03-24 23:26:21 +0500

    Issues Fixed

--- src/app/components/Events/Hero.jsx
+++ src/app/components/Events/Hero.jsx
@@ -2,37 +2,44 @@ import React from "react";
 import Image from "next/image";
 import { EventButton } from "../shared/button";
 import { FaArrowRight } from "react-icons/fa6";
-import pic1 from "../../assets/events/hero-pic-1.png";
-import pic2 from "../../assets/events/hero-pic-2.png";
-import pic3 from "../../assets/events/hero-pic-3.png";
+
 import heroBg from "../../assets/events/hero-event.png";
 import orangeSvg from "../../assets/events/svg-circle.png";
 import mouse from "../../assets/events/mouse.png";
 import triangle from "../../assets/events/triangle.png";
 import clock from "../../assets/events/clock.png";
-const Hero = () => {
+import square from "../../assets/events/square.svg";
+const Hero = ({ title, subTitleHead, subTitle, content, img1, img2, img3 }) => {
   return (
     <>
       <section className="w-[90%] mx-auto    xl:h-[calc(100vh-60px)] wraper-events  ">
         <div className="flex flex-col xl:flex-row  ">
-          <div className=" h-full w-full px-8  py-12 bg-secondary xl:flex-[2]">
+          <div className=" h-full sm:h-[50%] xl:h-full  w-full px-8  py-12 bg-secondary xl:flex-[2] realtive">
             <div className="flex flex-col">
-              <h2 className="text-primary font-bold text-[40px]   ">
-                DEEPFAKE WORKSHOP
+              <h2 className="text-primary font-bold text-[40px]   relative">
+                {title}
+                <Image
+                  src={square}
+                  alt="clock"
+                  width={30}
+                  height={30}
+                  className="absolute -top-5 -right-0"
+                />
               </h2>
-              <h3 className="text-white font-semibold text-[34px]">
-                <span className="text-primary">WEEKEND UX </span>providing the
-                best opportunities to the students around the glob.
+              <h3 className="text-white font-semibold text-[30px] leading-10 relative">
+                <span className="text-primary">{subTitleHead} </span>providing
+                the
+                {subTitle}
+                <span className="absolute top-1/2 -right-5 xl:-right-10">
+                  {" "}
+                  <Image src={mouse} alt="mouse" width={30} height={30} />
+                </span>
               </h3>
-              <p className="text-white text-[16px] font-light">
-                Weekend UX, is a UI/UX Design Academy in Delhi involved in User
-                Experience and User Interface Training and Consulting. It was
-                started in 2023 and passionate towards User Interface Design/
-                User Experience Design, Human Computer Interaction Design.
-                Humanoid is gushing towards competence to acquire knowledge and
-                have a wide understanding towards the sphere through the
-                foremost courses in the area of UI/UX Design, by strengthening
-                up your skills, for your golden future
+              <p className="text-white text-[16px] font-light text-justify sm:text-start mt-[10px] relative">
+                {content}
+                <span className="absolute bottom-1  xl:top-1/2 right-0">
+                  <Image src={triangle} alt="triangle" width={20} height={20} />
+                </span>
               </p>
               <EventButton
                 styles={"mt-2  justify-center  flex items-center gap-1"}
@@ -42,21 +49,21 @@ const Hero = () => {
               </EventButton>
             </div>
           </div>
-          <div className="w-full div-2-events bg-secondary flex-[0.5] -z-30 hidden xl:flex "></div>
-          <div className="w-full xl:flex-[2] relative items-center h-full sm:h-[50%] xl:h-full">
+          <div className="w-full div-1-events bg-secondary flex-[0.5] -z-30 hidden xl:flex "></div>
+          <div className="w-full xl:flex-[2] relative items-center h-full sm:h-[55rem] xl:h-full">
             <div className="flex flex-col px-12 py-10 sm:py-[100px] xl:py-10 relative items-center  xl:items-start">
               <div className="relative w-[250px] sm:w-[400px] xl:w-[300px] h-[180px]  sm:h-[250px] xl:h-[180px] rounded-md">
                 <Image
-                  src={pic1}
-                  alt="pic1"
+                  src={img1}
+                  alt={title}
                   fill
                   className="object-cover w-full h-full rounded-md z-30"
                 />
               </div>
               <div className="relative w-[250px] sm:w-[400px]  xl:w-[300px] h-[180px] sm:h-[250px] xl:h-[180px] -translate-y-5  -translate-x-20 sm:-translate-x-40 rounded-md">
                 <Image
-                  src={pic2}
-                  alt="pic1"
+                  src={img2}
+                  alt={subTitleHead}
                   fill
                   className="object-cover w-full h-full rounded-md z-20"
                 />
@@ -80,8 +87,8 @@ const Hero = () => {
 
               <div className="relative  w-[250px] sm:w-[400px] xl:w-[300px] h-[180px] sm:h-[250px] xl:h-[180px] rounded-md -translate-y-[50px] sm:-translate-y-[400px]  xl:-translate-y-[280px] sm:translate-x-20 ">
                 <Image
-                  src={pic3}
-                  alt="pic1"
+                  src={img3}
+                  alt={subTitle}
                   fill
                   className="object-cover w-full h-full rounded-md z-30"
                 />
