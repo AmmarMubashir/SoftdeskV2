b08f2e3d20d6db795f91dd02d51a6705b14082f5
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-03-27 22:25:48 +0500

    Data Updated

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -7,7 +7,11 @@ import pic3 from "../assets/events/hero-pic-3.png";
 import pic1a from "../assets/events/event-2-pic-1.jpg";
 import pic2a from "../assets/events/event-2-pic-2.jpg";
 import pic3a from "../assets/events/event-2-pic-3.jpg";
+import pic1b from "../assets/events/event-3-pic-1.jpg";
+import pic2b from "../assets/events/event-3-pic-2.jpg";
+import pic3b from "../assets/events/event-3-pic-3.jpg";
 import Event from "../components/Events/Event";
+
 const hero1 = {
   title: " DEEPFAKE WORKSHOP",
   subTitleHead: "WEEKEND UX",
@@ -24,6 +28,18 @@ const hero1 = {
   img2: pic2,
   img3: pic3,
 };
+
+const event1 = {
+  title: "TECHNOMELA",
+  subTitleHead: "TechnoMela",
+  subTitle: "to help students learn the art of personal branding",
+  content:
+    "Weekend UX, is a UI/UX Design Academy in Delhi involved in User Experience and User Interface Training and Consulting. It was started in 2023 and passionate towards User Interface Design/ User Experience Design, Human Computer Interaction Design. Humanoid is gushing towards competence to acquire knowledge and have a wide understanding towards the sphere through the foremost courses in the area of UI/UX Design, by strengthening up your skills, for your golden future",
+  img1: pic1b,
+  img2: pic2b,
+  img3: pic3b,
+};
+
 const hero2 = {
   title: " ICEBREAKING 2K21",
   subTitleHead: "WEEKEND UX",
@@ -53,13 +69,13 @@ const Events = () => {
         img3={hero1.img3}
       />
       <Event
-        title={hero1.title}
-        subTitleHead={hero1.subTitleHead}
-        subTitle={hero1.subTitle}
-        content={hero1.content}
-        img1={hero1.img1}
-        img2={hero1.img2}
-        img3={hero1.img3}
+        title={event1.title}
+        subTitleHead={event1.subTitleHead}
+        subTitle={event1.subTitle}
+        content={event1.content}
+        img1={event1.img1}
+        img2={event1.img2}
+        img3={event1.img3}
       />
       <Hero
         title={hero2.title}
