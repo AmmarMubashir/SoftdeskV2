4ccaf70df01ce238b6f961e9536f4693f4259485
Ammar <muhammadammarmubashir@gmail.com>
2024-04-07 18:04:03 +0500

    navbar updated

--- src/app/components/Home/Courses.js
+++ src/app/components/Home/Courses.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Slider from "./Slider";
+import spiral from "../../assets/home/spiral.png";
+import Image from "next/image";
 
 const Courses = () => {
   return (
     <div id="courses" className="min-h-[80vh] w-full ">
+      <Image
+        src={spiral}
+        height={100}
+        width={100}
+        alt="spiral courses"
+        className="absolute"
+      />
       <div className="flex flex-col gap-9 w-[90%] mx-auto">
         <div className="mx-auto text-center w-[60%]">
-          <h2 className=" text-[1.3rem] sm:text-[1.8rem] font-bold  text-black">
+          <h2 className=" text-[1.3rem] sm:text-[1.8rem] pt-3 font-bold  text-black">
             Courses <span className="text-[#FF7420]">Tech Spectrum</span>
           </h2>
           <p className="h-[2px] w-[50px] bg-[#FF7420] mx-auto translate-x-[-80px] sm:translate-x-[-140px] translate-y-[10px]"></p>
--- src/app/components/layout/Navbar.js
+++ src/app/components/layout/Navbar.js
@@ -8,9 +8,14 @@ import Link from "next/link";
 import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
-  const [show, Setshow] = useState(false);
+  const [showDrawer, setShowDrawer] = useState(false); // State variable to control drawer visibility
+
+  const closeDrawer = () => {
+    setShowDrawer(false);
+  };
+
   return (
-    <div className="h-[60px] bg-[#191A19] w-full flex justify-between relative  z-50">
+    <div className="h-[60px] bg-[#191A19] w-full flex justify-between relative z-50">
       <div className="p-4">
         <Image
           src={Logo}
@@ -20,69 +25,116 @@ const Navbar = () => {
           className="h-[50px] w-[60px] md:h-[70px] md:w-[90px]"
         />
       </div>
-      <div className=" h-[60px] w-[90%] sm:w-[50%] md:w-[80%] lg:w-[70%] text-white bg-[#191A19] shadow-lg shadow-black rounded-full rounded-tr-none rounded-br-none  flex items-center justify-between px-8 py-4">
-        <div className=" hidden md:flex bg-inherit gap-5 ">
-          <Link href="/" className="cursor-pointer bg-inherit">
+      <div className="h-[60px] w-[90%] sm:w-[50%] md:w-[80%] lg:w-[70%] text-white bg-[#191A19] shadow-lg shadow-black rounded-full rounded-tr-none rounded-br-none flex items-center justify-between px-8 py-4">
+        <div className="hidden md:flex bg-inherit gap-5">
+          <Link
+            href="/"
+            className="cursor-pointer bg-inherit"
+            onClick={closeDrawer}
+          >
             Home
           </Link>
-          <Link href="/#about" className="cursor-pointer bg-inherit">
+          <Link
+            href="/#about"
+            className="cursor-pointer bg-inherit"
+            onClick={closeDrawer}
+          >
             About Us
           </Link>
-          <Link href="/#cabinet" className="cursor-pointer bg-inherit">
+          <Link
+            href="/#cabinet"
+            className="cursor-pointer bg-inherit"
+            onClick={closeDrawer}
+          >
             Cabinet
           </Link>
-          <Link href="/#courses" className="cursor-pointer bg-inherit">
+          <Link
+            href="/#courses"
+            className="cursor-pointer bg-inherit"
+            onClick={closeDrawer}
+          >
             Courses
           </Link>
-          <Link href="/events" className="cursor-pointer bg-inherit">
+          <Link
+            href="/events"
+            className="cursor-pointer bg-inherit"
+            onClick={closeDrawer}
+          >
             Events
           </Link>
-          <Link href="/gallery" className="cursor-pointer bg-inherit">
+          <Link
+            href="/gallery"
+            className="cursor-pointer bg-inherit"
+            onClick={closeDrawer}
+          >
             Gallery
           </Link>
         </div>
         {/*  */}
-
         <Link
           href="/contact"
-          className=" bg-[#FF7420] px-3 rounded-xl h-[40px] flex items-center"
+          className="bg-[#FF7420] px-3 rounded-xl h-[40px] flex items-center"
+          onClick={closeDrawer}
         >
           Contact Us
         </Link>
         <GiHamburgerMenu
           className="md:hidden text-[30px] text-white"
-          onClick={() => Setshow(!show)}
+          onClick={() => setShowDrawer(!showDrawer)}
         />
       </div>
 
       {
         <div
-          // className="absolute z-10 top-[-6px] right-0 h-full w-[80%] md:w-[60%] bg-white text-black flex justify-center "
-          className={`fixed z-[100] top-0  left-0 md:hidden w-[300px] h-screen bg-white  overflow-hidden transition-transform transform ${
-            show ? "translate-x-0" : "-translate-x-full"
+          className={`fixed z-[100] top-0 left-0 md:hidden w-[300px] h-screen bg-white overflow-hidden transition-transform transform ${
+            showDrawer ? "translate-x-0" : "-translate-x-full"
           }`}
         >
           <IoClose
-            className=" text-[30px] text-black absolute right-4 top-4"
-            onClick={() => Setshow(!show)}
+            className="text-[30px] text-black absolute right-4 top-4"
+            onClick={() => setShowDrawer(false)}
           />
           <div className="p-4">
-            <Link href="/" className="block py-2 text-black">
+            <Link
+              href="/"
+              className="block py-2 text-black"
+              onClick={closeDrawer}
+            >
               Home
             </Link>
-            <Link href="/#about" className="block py-2 text-black">
+            <Link
+              href="/#about"
+              className="block py-2 text-black"
+              onClick={closeDrawer}
+            >
               About Us
             </Link>
-            <Link href="/#cabinet" className="block py-2 text-black">
+            <Link
+              href="/#cabinet"
+              className="block py-2 text-black"
+              onClick={closeDrawer}
+            >
               Cabinet
             </Link>
-            <Link href="/#courses" className="block py-2 text-black">
+            <Link
+              href="/#courses"
+              className="block py-2 text-black"
+              onClick={closeDrawer}
+            >
               Courses
             </Link>
-            <Link href="/events" className="block py-2 text-black">
+            <Link
+              href="/events"
+              className="block py-2 text-black"
+              onClick={closeDrawer}
+            >
               Events
             </Link>
-            <Link href="/gallery" className="block py-2 text-black">
+            <Link
+              href="/gallery"
+              className="block py-2 text-black"
+              onClick={closeDrawer}
+            >
               Gallery
             </Link>
           </div>
