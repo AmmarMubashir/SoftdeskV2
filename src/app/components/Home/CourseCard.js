83aa9008ffe182a33841e611469b8ac1b672e3d5
Ammar <muhammadammarmubashir@gmail.com>
2024-04-09 12:53:58 +0500

    course card modified

--- src/app/components/Home/CourseCard.js
+++ src/app/components/Home/CourseCard.js
@@ -4,31 +4,43 @@ import design from "../../assets/home/CourseCard/design.png";
 import { HiDocumentDownload } from "react-icons/hi";
 import { Avatar, Rating } from "@mui/material";
 
-const CourseCard = () => {
+const CourseCard = ({ data }) => {
+  const {
+    courseImageUrl,
+    courseTitle,
+    courseHeading,
+    courseDocumentUrl,
+    courseDescription,
+    courseHeadName,
+    courseHeadSession,
+    courseHeadImgUrl,
+  } = data;
   return (
     <div className="flex justify-between">
       <div className="w-[320px] xl:w-[310px] flex flex-col gap-3 bg-white p-3 rounded shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
         <div className="">
           <Image
-            src={design}
+            src={courseImageUrl}
             height={250}
             width={300}
             alt="design"
-            className="mx-auto"
+            className="mx-auto h-[200px] w-[300px]"
           />
         </div>
         <div>
-          <span className="text-[#1A906B]">Design</span>
+          <span className="text-[#1A906B]">{courseTitle}</span>
         </div>
         <div className="flex justify-between items-center">
-          <h3>UI/UX Figma</h3>
+          <h3>{courseHeading}</h3>
           <button className="h-[3rem] w-[3rem]">
-            <HiDocumentDownload className="h-[3rem] w-[3rem] text-[#00CE62]" />
+            <a href={courseDocumentUrl} download>
+              <HiDocumentDownload className="h-[3rem] w-[3rem] text-[#00CE62]" />
+            </a>
           </button>
         </div>
         <div>
-          <p className="text-[#667085]">
-            Learn how to apply development principles to your website designs.
+          <p className="text-[#667085] text-sm h-[100px]">
+            {courseDescription}
           </p>
         </div>
         <div className="flex items-center">
@@ -43,18 +55,27 @@ const CourseCard = () => {
         </div>
         <div className="flex justify-between">
           <div className="flex gap-2">
-            <Avatar
+            {/* <Avatar
               alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg"
+              src={courseHeadImgUrl}
               sx={{ width: 50, height: 50 }}
+            /> */}
+            <Image
+              src={courseHeadImgUrl}
+              height={50}
+              width={50}
+              alt="Course Head"
+              className="rounded-full h-[50px] w-[50px]"
             />
             <div className="flex flex-col gap-0">
-              <h3 className="text-md">Arslan Niazi</h3>
-              <p className="text-[#667085] text-sm">20 session SED</p>
+              <h3 className="text-sm">{courseHeadName}</h3>
+              <p className="text-[#667085] text-sm">{courseHeadSession}</p>
             </div>
           </div>
-          <button className="bg-[#FF7420] text-white px-2 py-1 rounded">
-            Download
+          <button className="bg-[#FF7420] text-white px-1 h-[40px] rounded">
+            <a href={courseDocumentUrl} download>
+              Download
+            </a>
           </button>
         </div>
       </div>
--- src/app/components/Home/Slider.js
+++ src/app/components/Home/Slider.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -12,6 +12,111 @@ import "./styles.css";
 // import required modules
 import { Autoplay, Pagination, Keyboard, Mousewheel } from "swiper/modules";
 import CourseCard from "./CourseCard";
+
+import WebDesigningImg from "../../assets/home/CourseCard/WebDesignImg.jpg";
+import DataScienceImg from "../../assets/home/CourseCard/DataScienceImg.jpg";
+import WebDevImg from "../../assets/home/CourseCard/WebDevImg.jpg";
+import CyberImg from "../../assets/home/CourseCard/CyberImg.jpg";
+import CloudImg from "../../assets/home/CourseCard/CloudImg.jpg";
+import FlutterImg from "../../assets/home/CourseCard/FlutterImg.jpg";
+
+import DataScienceHead from "../../assets/home/CourseCard/DataScienceHead2.jpeg";
+import WebDevHead from "../../assets/home/CourseCard/WebDevHead.jpg";
+import CloudHead from "../../assets/home/CourseCard/CloudHead2.jpg";
+import CyberHead from "../../assets/home/CourseCard/CyberHead.jpeg";
+import WebDevHead2 from "../../assets/home/CourseCard/WebHead2.jpeg";
+
+const courseData = [
+  {
+    courseImageUrl: WebDesigningImg,
+    courseTitle: "Design",
+    courseHeading: "UI/UX Figma",
+    courseDocumentUrl:
+      "https://magnetic-opal-835.notion.site/UI-UX-Roadmap-07b95de2056a40f5bcd48585fb403b9c",
+    courseDescription:
+      "Learn how to apply development principles to your website designs.",
+    courseHeadName: "Arslan Niazi",
+    courseHeadSession: "20 session SED",
+    courseHeadImgUrl: "",
+  },
+  {
+    courseImageUrl: DataScienceImg,
+    courseTitle: "Data",
+    courseHeading: "Data Science",
+    courseDocumentUrl: "DataScienceRoadmap.pdf",
+    courseDescription:
+      "Dive into the world of data science, where every dataset holds the keys to unlock infinite possibilities and insights.",
+    courseHeadName: "Syed Ali Taqi",
+    courseHeadSession: "21 session SED",
+    courseHeadImgUrl: DataScienceHead,
+  },
+  {
+    courseImageUrl: WebDevImg,
+    courseTitle: "Development",
+    courseHeading: "Full Stack Development",
+    courseDocumentUrl: "WebDevelopmentRoadmap.pdf",
+    courseDescription:
+      "Dive into the world of full-stack web development, where each click becomes a solution, sparking innovation and exploration.",
+    courseHeadName: "M.Ammar Mubashir",
+    courseHeadSession: "21 session SED",
+    courseHeadImgUrl: WebDevHead,
+  },
+  {
+    courseImageUrl: DataScienceImg,
+    courseTitle: "Data",
+    courseHeading: "Data Science",
+    courseDocumentUrl: "DataScienceRoadmap.pdf",
+    courseDescription:
+      "Dive into the world of data science, where every dataset contains valuable information waiting to be discovered and used to solve problems.",
+    courseHeadName: "Abdullah",
+    courseHeadSession: "21 session SED",
+    courseHeadImgUrl: "",
+  },
+  {
+    courseImageUrl: CyberImg,
+    courseTitle: "Security",
+    courseHeading: "Cyber Security",
+    courseDocumentUrl: "CyberSecurityRoadmap.pdf",
+    courseDescription:
+      "Learn the fundamental concepts of Cyber Security and Ethical Hacking from scratch.",
+    courseHeadName: "M.Abdur Raffay",
+    courseHeadSession: "21 session SED",
+    courseHeadImgUrl: CyberHead,
+  },
+  {
+    courseImageUrl: WebDevImg,
+    courseTitle: "Development",
+    courseHeading: "Full Stack Development",
+    courseDocumentUrl: "DataScienceRoadmap.pdf",
+    courseDescription:
+      "Dive into the world of full-stack web development, where every interaction sparks innovative solutions and fosters exploration.",
+    courseHeadName: "Syed Moazam Ali",
+    courseHeadSession: "21 session SED",
+    courseHeadImgUrl: WebDevHead2,
+  },
+  {
+    courseImageUrl: CloudImg,
+    courseTitle: "Cloud",
+    courseHeading: "Cloud Computing",
+    courseDocumentUrl: "CloudComputingRoadmap.pdf",
+    courseDescription:
+      "Master cloud computing by grasping its core concepts, understanding its infrastructure, and unlocking its transformative potential.",
+    courseHeadName: "Hamza Sajid",
+    courseHeadSession: "21 session SED",
+    courseHeadImgUrl: CloudHead,
+  },
+  {
+    courseImageUrl: FlutterImg,
+    courseTitle: "Development",
+    courseHeading: "App Development",
+    courseDocumentUrl: "FlutterRoadmap.pdf",
+    courseDescription:
+      "Dive into the world of full-stack web development, where every interaction sparks innovative solutions and fosters exploration.",
+    courseHeadName: "Faheem Abbass",
+    courseHeadSession: "20 session SED",
+    courseHeadImgUrl: "",
+  },
+];
 
 const Slider = () => {
   return (
@@ -41,7 +146,12 @@ const Slider = () => {
         modules={[Autoplay, Pagination, Keyboard, Mousewheel]}
         className=" p-5"
       >
-        <SwiperSlide>
+        {courseData.map((data) => (
+          <SwiperSlide key={data.courseHeadName}>
+            <CourseCard data={data} />
+          </SwiperSlide>
+        ))}
+        {/* <SwiperSlide>
           <CourseCard />
         </SwiperSlide>
         <SwiperSlide>
@@ -58,7 +168,7 @@ const Slider = () => {
         </SwiperSlide>
         <SwiperSlide>
           <CourseCard />
-        </SwiperSlide>
+        </SwiperSlide> */}
       </Swiper>
     </>
   );
